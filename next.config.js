6dc2ef288322fd2ddfef4f928bbe2e5b8efd8aa0
Maleesha Amendra <72781485+Maleesh3038@users.noreply.github.com>
2026-07-16 10:27:06 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      { source: '/wedding', destination: '/' },
+      { source: '/wedding/admin', destination: '/admin' },
+    ];
+  },
+};
 
 module.exports = nextConfig;
